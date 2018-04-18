4e4a0d34a8939c01951ff73b850659b0c3072215
Johanna Latt <j.latt@sap.com>
2018-04-18 12:34:41 -0400

    running threads as daemons to make sure threads are killed when process ends

--- Server/__init__.py
+++ Server/__init__.py
@@ -11,11 +11,13 @@ app = Flask(__name__)
 
 Messaging.init()
 thread = threading.Thread(target=Messaging.start_sending)
+thread.daemon = True
 thread.start()
 
 # Initiate the modules before starting to consume messages
 ModuleManager.initiate_modules(Messaging)
 thread = threading.Thread(target=Messaging.start_consuming)
+thread.daemon = True
 thread.start()
 
 app.run(host='0.0.0.0', port=PORT, debug=False)
