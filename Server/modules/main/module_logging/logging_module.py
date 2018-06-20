d17924ef462a4f0e4c8467de3242908f163340d3
Johanna Latt <j.latt@sap.com>
2018-06-20 15:51:45 -0400

    adding logger module

--- Server/modules/main/module_logging/logging_module.py
+++ Server/modules/main/module_logging/logging_module.py
@@ -1,20 +1,44 @@
 from Server.modules.abstract_mirror_module import AbstractMirrorModule
 
+import logging
+from logging.handlers import RotatingFileHandler
+import datetime
+import os
 
-class Logging(AbstractMirrorModule):
+max_number_of_log_files = 20
 
-    def mirror_started(self):
-        super().mirror_started()
-        pass
 
-    def tracking_started(self):
-        super().tracking_started()
-        pass
+class LoggingModule(AbstractMirrorModule):
 
-    def tracking_data(self, data):
-        super().tracking_data(data)
-        # do nothing with that
-        pass
+    def __init__(self, Messaging, queue, User):
+        super().__init__(Messaging, queue, User)
 
-    def tracking_lost(self):
-        pass
+        self.delete_old_log_files()
+
+        self.raw_tracking_data_logger = logging.getLogger('Smart-Health-Mirror-Raw-Data')
+
+        now = datetime.datetime.now()
+        filename = "logs/{}_raw_tracking_data.log".format(now.strftime('%Y_%m_%d-%H_%M_%S'))
+        hdlr = RotatingFileHandler(filename, maxBytes=5*1024*1024)  # max 5 MB
+
+        formatter = logging.Formatter('%(message)s')
+        hdlr.setFormatter(formatter)
+
+        self.raw_tracking_data_logger.addHandler(hdlr)
+
+    def user_skeleton_updated(self, user):
+        super().user_skeleton_updated(user)
+        self.raw_tracking_data_logger.error(user.get_joints())
+
+    def delete_old_log_files(self):
+        files = os.listdir('logs/')
+
+        # Filter out hidden files from the system
+        files = [x for x in files if not x.startswith(".")]
+
+        if len(files) > (max_number_of_log_files - 1):
+            files.sort()   # Oldest will be first
+            print("range is 0 to {}".format(len(files) - max_number_of_log_files + 1))
+            for i in range(0, (len(files) - max_number_of_log_files + 1)):
+                f = os.path.join('logs/', files[i])
+                os.remove(f)
