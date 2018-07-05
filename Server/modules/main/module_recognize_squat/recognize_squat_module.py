7ced033f64ccc37b80a8bd9a7196228ab241753e
Johanna Latt <j.latt@gmx.de>
2018-07-05 16:34:02 -0400

    greatly improving recognize squat module * using data over time to determine if user is walking away not * using user specific measurements (todo: test with realtime data)

--- Server/modules/main/module_recognize_squat/recognize_squat_module.py
+++ Server/modules/main/module_recognize_squat/recognize_squat_module.py
@@ -6,26 +6,33 @@ from Server.user import USER_STATE
 from Server.user import SQUAT_STAGE
 
 import json
+from collections import deque
+
+import numpy as np
 
 
 class RecognizeSquatModule(AbstractMirrorModule):
 
-    threshold_straight_x = 30
-    threshold_straight_z = 70
+    threshold_equal_y_pos = 20     # maximum wiggle room in y to consider two y-positions as the same
+    threshold_movement_in_y = None   # minimum distance needed in y to count something as a squat (will be calculated per user)
 
-    threshold_straight_squatting_x = 60
+    timeseries_length = 50
+    max_std_x = 40
+    max_std_z = 100
 
-    threshold_in_place_x = 350
-    threshold_in_place_z = 600
-    threshold_equal_y_pos = 20
-    threshold_movement_in_y = 80
+    red = (1, 0.2, 0.2, 1)
 
     def __init__(self, Messaging, queue, User):
         super().__init__(Messaging, queue, User)
+
+        # Save the spine_base and spine_shoulder position for the last 20 frames
+        self.pos_spine_base = deque([[0,0,0]] * self.timeseries_length, maxlen=self.timeseries_length)
+        self.pos_spine_shoulder = deque([[0,0,0]] * self.timeseries_length, maxlen=self.timeseries_length)
+
         self.__reset_variables()
 
     def __reset_variables(self):
-        self.standing_straight = False
+        self.ready_to_squat = False
         self.squatting = False
         self.squat_completed = False
         self.just_finished_squat = False
@@ -38,8 +45,6 @@ class RecognizeSquatModule(AbstractMirrorModule):
 
         self.repetitions = 0
 
-        self.red = (1, 0.2, 0.2, 1)
-
         self.User.update_user_state_and_stage(USER_STATE.NONE, SQUAT_STAGE.NONE)
 
     def mirror_started(self):
@@ -49,85 +54,80 @@ class RecognizeSquatModule(AbstractMirrorModule):
     def user_skeleton_updated(self, user):
         super().user_skeleton_updated(user)
 
-        # Get the relevant joints
+        # Get the relevant joints and save them
         joints = user.get_joints()
         spine_shoulder = joints[KINECT_JOINTS.SpineShoulder.name]
         spine_base = joints[KINECT_JOINTS.SpineBase.name]
         spine_mid = joints[KINECT_JOINTS.SpineMid.name]
 
-        # Check if READY FOR SQUAT
-        if self.is_upper_body_straight(spine_shoulder, spine_mid, spine_base) and not self.standing_straight:
-            self.standing_straight = True
-            self.starting_spine_shoulder_pos = spine_shoulder
-            self.starting_spine_base_pos = spine_base
+        self.pos_spine_base.append(spine_base)
+        self.pos_spine_shoulder.append(spine_shoulder)
 
-        if self.standing_straight:
-            # Check for walking away
-            if (self.squatting and
-            (abs(self.starting_spine_base_pos[0] - spine_base[0]) > self.threshold_in_place_x or
-            abs(self.starting_spine_base_pos[2] - spine_base[2]) > self.threshold_in_place_z)):
-                self.repetitions = 0
-                self.squatting = False
-                self.standing_straight = False
-                self.send_to_mirror("squat_repetitions", "Repetitions: {}".format(self.repetitions), stay=1)
-                self.send_to_mirror("squat_text", "Squat failed, you walked - resetting", self.red, stay=1)
+        if self.is_ready_to_squat():
+            # Calculate user specific measurements
+            if self.threshold_movement_in_y is None:
+                self.threshold_movement_in_y = (spine_shoulder[1] - spine_base[1]) / 5
+                print(self.threshold_movement_in_y)
 
-                self.User.update_user_state_and_stage(USER_STATE.NONE, SQUAT_STAGE.NONE)
+            # At the very beginning of the squat, save starting position
+            if not self.ready_to_squat:
+                self.ready_to_squat = True
+                self.starting_spine_shoulder_pos = spine_shoulder
+                self.starting_spine_base_pos = spine_base
 
-            # Check for straight back
-            elif self.is_upper_body_straight_during_squat(spine_shoulder, spine_mid, spine_base):
-                # Moving down -> SQUAT STARTED
-                if (self.starting_spine_shoulder_pos[1] - spine_shoulder[1]) > self.threshold_movement_in_y:
-                    self.squatting = True
-                    self.just_finished_squat = False
-                    self.send_to_mirror("squat_text", "You are doing squats - keep on going!")
-                    self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.NONE)
+            # Moving down -> SQUAT STARTED
+            if (self.starting_spine_shoulder_pos[1] - spine_shoulder[1]) > self.threshold_movement_in_y:
+                # print("[RecognizeSquatModule][info] Squatting")
+                self.squatting = True
+                self.just_finished_squat = False
+                self.send_to_mirror("squat_text", "You are doing squats - keep on going!")
+                self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.NONE)
 
-                # Determine squat direction
-                if self.squatting:
-                    current_distance_in_y = self.starting_spine_shoulder_pos[1] - spine_shoulder[1]
-                    if (self.last_distance_in_y < current_distance_in_y):
-                        self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.GOING_DOWN)
-                    else:
-                        self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.GOING_UP)
+            # Determine squat direction
+            if self.squatting:
+                current_distance_in_y = self.starting_spine_shoulder_pos[1] - spine_shoulder[1]
+                if (self.last_distance_in_y < current_distance_in_y):
+                    self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.GOING_DOWN)
+                else:
+                    self.User.update_user_state_and_stage(USER_STATE.SQUATTING, SQUAT_STAGE.GOING_UP)
 
-                # After moving down, moved back up -> SQUAT COMPLETE
-                if not self.just_finished_squat and self.squatting and abs(self.starting_spine_shoulder_pos[1] - spine_shoulder[1]) < self.threshold_equal_y_pos:
-                    self.just_finished_squat = True
-                    self.repetitions += 1
-                    self.send_to_mirror("squat_repetitions", "Repetitions: {}".format(self.repetitions))
-
-            # SQUAT FAILED
-            else:
-                self.squatting = False
-                self.standing_straight = False
-                self.repetitions = 0
-                self.send_to_mirror("squat_text", "Squat failed, your back has to be straight - resetting", self.red, stay=1)
-                self.send_to_mirror("squat_repetitions", "Repetitions: {}".format(self.repetitions), stay=1)
-
-                self.User.update_user_state_and_stage(USER_STATE.NONE, SQUAT_STAGE.NONE)
+            # After moving down, moved back up -> SQUAT COMPLETE
+            if not self.just_finished_squat and self.squatting and abs(self.starting_spine_shoulder_pos[1] - spine_shoulder[1]) < self.threshold_equal_y_pos:
+                self.just_finished_squat = True
+                self.repetitions += 1
+                self.send_to_mirror("squat_repetitions", "Repetitions: {}".format(self.repetitions))
 
             # Save the current distance to the initial spine position
             self.last_distance_in_y = self.starting_spine_shoulder_pos[1] - spine_shoulder[1]
 
-    # Checks if the upper body is straight while the user is not moving
-    def is_upper_body_straight(self, spine_shoulder, spine_mid, spine_base):
-        if (abs(spine_shoulder[0] - spine_mid[0]) < self.threshold_straight_x
-        and abs(spine_mid[0] - spine_base[0]) < self.threshold_straight_x
-        and abs(spine_shoulder[2] - spine_mid[2]) < self.threshold_straight_z
-        and abs(spine_mid[2] - spine_base[2]) < self.threshold_straight_z):
-            return True
-        return False
+        elif self.squatting:
+            # Walked away
+            print("[RecognizeSquatModule][info] Walked away")
+            self.repetitions = 0
+            self.ready_to_squat = False
+            self.squatting = False
+            self.standing_straight = False
+            self.send_to_mirror("squat_repetitions", "Repetitions: {}".format(self.repetitions), stay=1)
+            self.send_to_mirror("squat_text", "Squat failed, you walked - resetting", self.red, stay=1)
 
-    # Checks if the upper body stays straight during a squat - has some more
-    # wiggle room than is_upper_body_straight since this method is intended
-    # to be used DURING a movement (so there is some natural shacking). It also
-    # only checks x since z should actually move during a squat.
-    def is_upper_body_straight_during_squat(self, spine_shoulder, spine_mid, spine_base):
-        if (abs(spine_shoulder[0] - spine_mid[0]) < self.threshold_straight_squatting_x
-        and abs(spine_mid[0] - spine_base[0]) < self.threshold_straight_squatting_x):
-            return True
-        return False
+            self.User.update_user_state_and_stage(USER_STATE.NONE, SQUAT_STAGE.NONE)
+
+    # Uses time series data of last x frames to determine if the user was
+    # standing in place, ie is ready for a squat. More specifically, we look
+    # at the standard deviation of the x- and z-position of two spine bones
+    # over the last x frames
+    def is_ready_to_squat(self):
+        std_spine_base_movement = np.std(np.asarray(self.pos_spine_base), axis=0)
+        std_spine_shoulder_movement = np.std(np.asarray(self.pos_spine_shoulder), axis=0)
+
+        return (std_spine_base_movement[0] < self.max_std_x
+                and std_spine_base_movement[2] < self.max_std_z
+                and std_spine_shoulder_movement[0] < self.max_std_x
+                and std_spine_shoulder_movement[2] < self.max_std_z)
+
+    def calculate_user_thresholds(self):
+        #TODO:
+        pass
 
     def tracking_lost(self):
         # print("[RecognizeSquatModule][info] Cleaning up")
