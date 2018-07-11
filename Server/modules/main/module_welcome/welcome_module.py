7a0d14a1ac5e55697959eb8a26164c22c306c3c7
Johanna Latt <j.latt@gmx.de>
2018-07-11 17:02:45 -0400

    ask user to step in front of the mirror if nobody is tracked

--- Server/modules/main/module_welcome/welcome_module.py
+++ Server/modules/main/module_welcome/welcome_module.py
@@ -2,6 +2,7 @@ from Server.modules.abstract_mirror_module import AbstractMirrorModule
 from Server.utils.enums import MSG_TO_MIRROR_KEYS
 
 import json
+import threading
 
 
 class WelcomeModule(AbstractMirrorModule):
@@ -9,43 +10,26 @@ class WelcomeModule(AbstractMirrorModule):
     def mirror_started(self):
         super().mirror_started()
         print("[WelcomeModule][info] Mirror is started")
-        self.Messaging.send_message(
-            MSG_TO_MIRROR_KEYS.TEXT.name, json.dumps({
-                                        "text": "Welcome!",
-                                        "position": {"x": 0, "y": 0},
-                                        "font_size": 47,
-                                        "halign": "center",
-                                        "id": "Welcome",
-                                        "animation": {
-                                            "fade_in": 1,
-                                            "stay": 5,
-                                            "fade_out": 3}
-                                        }))
+        self.Messaging.send_text_to_mirror("Welcome!", id="Welcome", position={"x": 0, "y": 0}, font_size=47, halign="center", fade_in = 1, stay=5, fade_out=2)
+        self.Messaging.send_text_to_mirror("Step in front of the mirror to exercise", position={"x": 0, "y": -0.07}, font_size=38, halign="center", fade_in = 1, stay=5, fade_out=2)
+
+        self.remind_user_timer = threading.Timer(7, self.motivate_user)
+        self.remind_user_timer.start()
 
     def tracking_started(self):
         super().tracking_started()
         print("[WelcomeModule][info] Tracking")
-        self.Messaging.send_message(
-            MSG_TO_MIRROR_KEYS.TEXT.name, json.dumps({
-                                        "text": "I can see you! :-)",
-                                        "position": {"x": 0.5, "y": 0.9},
-                                        "id": "Welcome",
-                                        "animation": {
-                                            "fade_in": 0.5,
-                                            "stay": 1,
-                                            "fade_out": 0.5}
-                                        }))
+        self.remind_user_timer.cancel()
+        self.Messaging.send_text_to_mirror("I can see you! :-)", id="Welcome", position={"x": 0, "y": 0.2}, font_size=38, halign="center", fade_in = 0.5, stay=1, fade_out=0.5)
 
     def tracking_lost(self):
         super().tracking_lost()
         print("[WelcomeModule][info] Tracking Lost")
-        self.Messaging.send_message(
-            MSG_TO_MIRROR_KEYS.TEXT.name, json.dumps({
-                                        "text": "I lost you :-(",
-                                        "position": {"x": 0.5, "y": 0.9},
-                                        "id": "Welcome",
-                                        "animation": {
-                                            "fade_in": 0.5,
-                                            "stay": 1,
-                                            "fade_out": 0.5}
-                                        }))
+        self.remind_user_timer = threading.Timer(5, self.motivate_user)
+        self.remind_user_timer.start()
+        self.Messaging.send_text_to_mirror("I lost you :-(", id="Welcome", position={"x": 0, "y": 0.2}, font_size=38, halign="center", fade_in = 0.5, stay=1, fade_out=0.5)
+
+    def motivate_user(self):
+        self.Messaging.send_text_to_mirror("Step in front of the mirror to exercise!", id="Welcome", position={"x": 0, "y": 0}, font_size=47, halign="center", fade_in = .5, stay=3, fade_out=.5)
+        self.remind_user_timer = threading.Timer(7, self.motivate_user)
+        self.remind_user_timer.start()
