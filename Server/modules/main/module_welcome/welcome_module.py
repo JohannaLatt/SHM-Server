1747fc635ce9d58c7363105a65c7f06e72eb9b67
Johanna Latt <j.latt@gmx.de>
2018-07-12 10:38:36 -0400

    fixing welcome-timer not always being cancelled

--- Server/modules/main/module_welcome/welcome_module.py
+++ Server/modules/main/module_welcome/welcome_module.py
@@ -7,24 +7,34 @@ import threading
 
 class WelcomeModule(AbstractMirrorModule):
 
+    def __init__(self, Messaging, queue, User):
+        super().__init__(Messaging, queue, User)
+
+        self.remind_user_timer = None
+
     def mirror_started(self):
         super().mirror_started()
         print("[WelcomeModule][info] Mirror is started")
         self.Messaging.send_text_to_mirror("Welcome!", id="Welcome", position={"x": 0, "y": 0}, font_size=47, halign="center", fade_in = 1, stay=5, fade_out=2)
         self.Messaging.send_text_to_mirror("Step in front of the mirror to exercise", position={"x": 0, "y": -0.07}, font_size=38, halign="center", fade_in = 1, stay=5, fade_out=2)
 
+        if self.remind_user_timer is not None:
+            self.remind_user_timer.cancel()
         self.remind_user_timer = threading.Timer(7, self.motivate_user)
         self.remind_user_timer.start()
 
     def tracking_started(self):
         super().tracking_started()
         print("[WelcomeModule][info] Tracking")
-        self.remind_user_timer.cancel()
+        if self.remind_user_timer is not None:
+            self.remind_user_timer.cancel()
         self.Messaging.send_text_to_mirror("I can see you! :-)", id="Welcome", position={"x": 0, "y": 0.2}, font_size=38, halign="center", fade_in = 0.5, stay=1, fade_out=0.5)
 
     def tracking_lost(self):
         super().tracking_lost()
         print("[WelcomeModule][info] Tracking Lost")
+        if self.remind_user_timer is not None:
+            self.remind_user_timer.cancel()
         self.remind_user_timer = threading.Timer(5, self.motivate_user)
         self.remind_user_timer.start()
         self.Messaging.send_text_to_mirror("I lost you :-(", id="Welcome", position={"x": 0, "y": 0.2}, font_size=38, halign="center", fade_in = 0.5, stay=1, fade_out=0.5)
